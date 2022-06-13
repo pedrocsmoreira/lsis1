e5e882777d15e811be2e3eb5899834e3f4c31e57
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2022-06-13 22:25:42 +0100

    update sigc

--- SIGC/lsis1/src/main/java/com/lsis1/Main.java
+++ SIGC/lsis1/src/main/java/com/lsis1/Main.java
@@ -3,6 +3,8 @@ package com.lsis1;
 import io.vertx.core.Vertx;
 
 public class Main{
-    Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(new Server());
+    public Main(){
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new Server());
+    }
 }
