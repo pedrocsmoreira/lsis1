f8b45ea79d2435d004d20897d81ea2c403af0736
AlexGomes8 <107181314+AlexGomes8@users.noreply.github.com>
2022-06-15 19:47:49 +0100

    update cod motores

--- Arduino/motores/codigo_motores/codigo_motores.ino
+++ Arduino/motores/codigo_motores/codigo_motores.ino
@@ -1,62 +1,26 @@
-#define PWMA 3
-#define AIN2 12
-#define AIN1 11
-#define BIN1 9
-#define BIN2 8
-#define PWMB 5
+#include <analogWrite.h>
+
+#define PWM1 39
+#define PWM2 36
+#define DIR1 14
+#define DIR2 12
+
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 8;
 
 void setup(){
-    Serial.begin(9600);
-    pinMode(PWMA, OUTPUT);
-    pinMode(AIN2, OUTPUT);
-    pinMode(AIN1, OUTPUT);
-    pinMode(BIN1, OUTPUT);
-    pinMode(BIN2, OUTPUT);
-    pinMode(PWMB, OUTPUT);
+    pinMode(DIR1, OUTPUT);
+    pinMode(DIR2, OUTPUT);
+
+    ledcSetup(ledChannel, freq, resolution);
+    ledcAttachPin(PWM1, ledChannel);
+    ledcAttachPin(PWM2, ledChannel);
 }
 
 void loop(){
-    /*Serial.println("Drive");
-    drive();
-    delay(5000);
-    Serial.println("Stop");
-    stop();
-    delay(5000);
-    Serial.println("Drive");
-    drive();
-    delay(5000);
-    Serial.println("Brake");
-    brake();
-    delay(100000000);*/
-    analogWrite(PWMA, 255);
-    analogWrite(PWMB, 255);
-    digitalWrite(AIN1, LOW);
-    digitalWrite(AIN2, LOW);
-}
-
-void drive(){
-    analogWrite(PWMA, 255);
-    analogWrite(PWMB, 255);
-    digitalWrite(AIN1, HIGH);
-    digitalWrite(AIN2, LOW);
-    digitalWrite(BIN1, HIGH);
-    digitalWrite(BIN2, LOW);
-}
-
-void stop(){
-    analogWrite(PWMA, 0);
-    analogWrite(PWMB, 0);
-    digitalWrite(AIN1, LOW);
-    digitalWrite(AIN2, LOW);
-    digitalWrite(BIN1, LOW);
-    digitalWrite(BIN2, LOW);
-}
-
-void brake(){
-    analogWrite(PWMA, 255);
-    analogWrite(PWMB, 255);
-    digitalWrite(AIN1, HIGH);
-    digitalWrite(AIN2, HIGH);
-    digitalWrite(BIN1, HIGH);
-    digitalWrite(BIN2, HIGH);
+    ledcWrite(PWM1, 255);
+    ledcWrite(PWM2, 255);
+    digitalWrite(DIR1, LOW);
+    digitalWrite(DIR2, HIGH);
 }
